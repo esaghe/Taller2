68b024ab66f0a8ef9079c7804d0595a9db40fa53
esaghe <estefany_aguilar82171@elpoli.edu.co>
2019-06-05 11:51:46 -0500

    Taller2

---
+++ src/assets/data/heroes.js
@@ -0,0 +1,59 @@
+import aquaman from '../img/aquaman.png';
+import batman from '../img/batman.png';
+import daredevil from '../img/daredevil.png';
+import hulk from '../img/hulk.png';
+import linterna_verde from '../img/linterna_verde.png';
+import spiderman from '../img/spiderman.png';
+import wolverine from '../img/wolverine.png';
+
+export default[
+    {
+        nombre: "Aquaman",
+        bio: "Los rasgos principales de batman se resumen en destreza física, habilidades deductivas y obsesión. La mayor parte de las caracterísiticas básicas de los comics ",
+        img: aquaman,
+        aparicion: "",
+        casa: "DC",
+    },
+    {
+        nombre: "Batman",
+        bio: "Los rasgos principales de batman se resumen en destreza física, habilidades deductivas y obsesión. La mayor parte de las caracterísiticas básicas de los comics ",
+        img: batman,
+        aparicion: "",
+        casa: "DC",
+    },
+    {
+        nombre: "Daredevil",
+        bio: "Los rasgos principales de batman se resumen en destreza física, habilidades deductivas y obsesión. La mayor parte de las caracterísiticas básicas de los comics ",
+        img: daredevil,
+        aparicion: "",
+        casa: "Marvel",
+    },
+    {
+        nombre: "Hulk",
+        bio: "Los rasgos principales de batman se resumen en destreza física, habilidades deductivas y obsesión. La mayor parte de las caracterísiticas básicas de los comics ",
+        img: hulk,
+        aparicion: "",
+        casa: "Marvel",
+    },
+    {
+        nombre: "Spiderman",
+        bio: "Los rasgos principales de batman se resumen en destreza física, habilidades deductivas y obsesión. La mayor parte de las caracterísiticas básicas de los comics ",
+        img: spiderman,
+        aparicion: "",
+        casa: "Marvel",
+    },
+    {
+        nombre: "Wolverine",
+        bio: "Los rasgos principales de batman se resumen en destreza física, habilidades deductivas y obsesión. La mayor parte de las caracterísiticas básicas de los comics ",
+        img: wolverine,
+        aparicion: "",
+        casa: "Marvel",
+    },
+    {
+        nombre: "Linterna verde",
+        bio: "Los rasgos principales de batman se resumen en destreza física, habilidades deductivas y obsesión. La mayor parte de las caracterísiticas básicas de los comics ",
+        img: linterna_verde,
+        aparicion: "",
+        casa: "DC",
+    },
+];
